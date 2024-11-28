f1b904dc646666209950a144b9949fa6bbc3b727
Niestrat99 <niestrat99@gmail.com>
2024-11-28 20:40:53 +0100

    feat: Initiating API
    feat: Added function enableKeepInventory(Player player) to the API
    
    feat: Added function disableKeepInventory(Player player) to the API

---
+++ src/main/java/io/github/niestrat99/keepinvindividual/api/KeepInvAPI.java
@@ -0,0 +1,36 @@
+package io.github.niestrat99.keepinvindividual.api;
+
+import io.github.niestrat99.keepinvindividual.KeepInvIndividual;
+import io.github.niestrat99.keepinvindividual.configuration.KeepInvLocal;
+import io.github.niestrat99.keepinvindividual.utilities.CacheList;
+import org.bukkit.entity.Player;
+
+import java.util.logging.Level;
+
+public class KeepInvAPI implements IKeepInvAPI {
+    private final boolean sql = KeepInvIndividual.mySqlEnabled;
+
+    public void enableKeepInventory(Player player) {
+        if (sql) {
+            CacheList.addToList(player);
+        } else {
+            try {
+                KeepInvLocal.removeUniqueID(player);
+            } catch (Exception e) {
+                KeepInvIndividual.log(Level.SEVERE, "Something went wrong adding player to local list!", KeepInvAPI.class, e);
+            }
+        }
+    }
+
+    public void disableKeepInventory(Player player) {
+        if (sql) {
+            CacheList.removeFromList(player);
+        } else {
+            try {
+                KeepInvLocal.removeUniqueID(player);
+            } catch (Exception e) {
+                KeepInvIndividual.log(Level.SEVERE, "Something went wrong removing player from local list!", KeepInvAPI.class, e);
+            }
+        }
+    }
+}
